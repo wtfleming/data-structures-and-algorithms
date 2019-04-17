bb998b598aa76f4a1e6682f0b3aa18cead85bc20
Will Fleming <wfleming77@gmail.com>
2019-04-16 21:28:53 -0400

    arrays

--- typescript/src/daily-coding-problem/arrays.test.ts
+++ typescript/src/daily-coding-problem/arrays.test.ts
@@ -5,3 +5,8 @@ test('getProductOfAllOtherElements()', () => {
   expect(arrays.getProductOfAllOtherElements([1, 2, 3, 4, 5])).toEqual([120, 60, 40, 30, 24]);
   expect(arrays.getProductOfAllOtherElements([3, 2, 1])).toEqual([2, 3, 6]);
 });
+
+test('getProductOfAllOtherElementsNoDivision()', () => {
+  expect(arrays.getProductOfAllOtherElementsNoDivision([1, 2, 3, 4, 5])).toEqual([120, 60, 40, 30, 24]);
+  expect(arrays.getProductOfAllOtherElementsNoDivision([3, 2, 1])).toEqual([2, 3, 6]);
+});
--- typescript/src/daily-coding-problem/arrays.ts
+++ typescript/src/daily-coding-problem/arrays.ts
@@ -4,3 +4,42 @@ export function getProductOfAllOtherElements(arr: number[]) {
   const totalProduct = arr.reduce((acc, currentVal) => acc * currentVal);
   return arr.map(x => totalProduct / x);
 }
+
+// Same as above, but without using division
+export function getProductOfAllOtherElementsNoDivision(arr: number[]) {
+  const prefixProducts: number[] = [];
+  arr.forEach(num => {
+    if (prefixProducts.length > 0) {
+      prefixProducts.push(prefixProducts[prefixProducts.length - 1] * num);
+    } else {
+      prefixProducts.push(num);
+    }
+  });
+
+  // JS array.reverse() reverses in place, so create a new array instead
+  const reversedArray = [...arr].reverse();
+  const suffixProducts: number[] = [];
+  reversedArray.forEach(num => {
+    if (suffixProducts.length > 0) {
+      suffixProducts.push(suffixProducts[suffixProducts.length - 1] * num);
+    } else {
+      suffixProducts.push(num);
+    }
+  });
+
+  suffixProducts.reverse();
+
+  const result: number[] = [];
+
+  arr.forEach((_val, idx) => {
+    if (idx === 0) {
+      result.push(suffixProducts[idx + 1]);
+    } else if (idx === arr.length - 1) {
+      result.push(prefixProducts[idx - 1]);
+    } else {
+      result.push(suffixProducts[idx + 1] * prefixProducts[idx - 1]);
+    }
+  });
+
+  return result;
+}
