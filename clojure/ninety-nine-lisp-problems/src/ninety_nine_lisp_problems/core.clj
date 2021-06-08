3219153682e5f0cf606a5ac77c6805f34897adce
Will Fleming <wfleming77@gmail.com>
2021-06-07 21:44:04 -0400

    work on 99 lisp problems

--- clojure/ninety-nine-lisp-problems/src/ninety_nine_lisp_problems/core.clj
+++ clojure/ninety-nine-lisp-problems/src/ninety_nine_lisp_problems/core.clj
@@ -1,6 +1,39 @@
 (ns ninety-nine-lisp-problems.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn my-last
+  "Finds the last box of a list"
+  [coll]
+  (let [[h & t] coll]
+    (if t
+      (recur t)
+      h)))
+
+(defn my-but-last
+  "Find the last two boxes of a list"
+  [coll]
+  (->> coll
+      reverse
+      (take 2)
+      reverse))
+
+(defn element-at
+  "Find the K'th element of a list.
+  Note that unlike nth, the first element in the list is number 1."
+  [coll n]
+  (loop [[h & t] coll
+         acc 1]
+    (if (= acc n)
+      h
+      (recur t (inc acc)))))
+
+(defn my-count
+  "Find the number of elements of a list."
+  [coll]
+  (if (empty? coll)
+    0
+    (loop [coll coll
+           acc 1]
+      (if (next coll)
+        (recur (rest coll) (inc acc))
+        acc))))
+
--- clojure/ninety-nine-lisp-problems/test/ninety_nine_lisp_problems/core_test.clj
+++ clojure/ninety-nine-lisp-problems/test/ninety_nine_lisp_problems/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [ninety-nine-lisp-problems.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest my-last-test
+  (testing "Finds the last box in a list."
+    (is (= (my-last '(:a :b :c :d)) :d))))
+
+(deftest my-but-last-test
+  (testing "Finds the last but one box in a list."
+    (is (= (my-but-last '(:a :b :c :d)) '(:c :d)))))
+
+(deftest element-at-test
+  (testing "Finds the K'th element of a list."
+    (is (= (element-at '(:a :b :c :d :e) 3) :c)))
+  (testing "Returns nil if n is larger than the length of the list"
+    (is (= (element-at '(:a) 3) nil))))
+
+(deftest my-count-test
+  (testing "Find the number of elements of a list."
+    (is (= (my-count '(:a :b :c :d)) 4)))
+    (testing "Find the number of elements of an empty list."
+    (is (= (my-count '()) 0))))
+
